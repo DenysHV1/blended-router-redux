9a28c0fdc2aa3ff99789b40dc4f59ebb077b45b6
Sairus78 <83472347+Sairus78@users.noreply.github.com>
2024-11-23 17:52:36 +0200

    fin

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,27 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  CountryInfo,
+  GoBackBtn,
+  Heading,
+  Loader,
+  Section,
+} from 'components';
+import { useFetchCountry } from 'hoocks';
+import { useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Country = () => {
+  const { country, isLoading, error } = useFetchCountry();
+  const location = useLocation();
+  console.log(location);
+  const goBack = useRef(location?.state ?? '/');
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <GoBackBtn path={goBack.current}>Go back</GoBackBtn>
+        {error && <Heading title="Something went wrong!" bottom />}
+        {isLoading && <Loader />}
+        {country && <CountryInfo {...country} />}
       </Container>
     </Section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,13 @@ import { Container, CountryList, Heading, Loader, Section } from 'components';
 import { useFetchCountries } from 'hoocks';
 const Home = () => {
   const { countries, isLoading, error } = useFetchCountries();
-  console.log(countries);
 
   return (
     <Section>
       <Container>
         {error && <Heading title="Something went wrong!" bottom />}
         {isLoading && <Loader />}
-        {countries.length > 0 && <CountryList countries={countries}/>}
+        {countries.length > 0 && <CountryList countries={countries} />}
       </Container>
     </Section>
   );
